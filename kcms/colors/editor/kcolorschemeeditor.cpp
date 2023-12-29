6302673ebe9fb7fd02843859c4dbf84151be38dc
Kai Uwe Broulik <kde@privat.broulik.de>
2023-12-29 22:16:14 +0100

    kcms/colors: Attach color scheme editor to KCM window on Wayland, too
    Using the new KWaylandExtras::exportWindow functionality.
    
    Also drop the "x11" prefix from the window handle since realistically
    both KCM and editor will be running under the same windowing system.

--- kcms/colors/editor/kcolorschemeeditor.cpp
+++ kcms/colors/editor/kcolorschemeeditor.cpp
@@ -56,21 +56,11 @@ int main(int argc, char *argv[])
     SchemeEditorDialog dialog(path);
     dialog.setShowApplyOverwriteButton(parser.isSet(overwriteOption));
 
-    // FIXME doesn't work :(
     const QString attachHandle = parser.value(attachOption);
     if (!attachHandle.isEmpty()) {
-        // TODO wayland: once we have foreign surface support
-        const QString x11Prefix = QStringLiteral("x11:");
-
-        if (attachHandle.startsWith(x11Prefix)) {
-            bool ok = false;
-            WId winId = attachHandle.mid(x11Prefix.length()).toLong(&ok, 0);
-            if (ok) {
-                dialog.setModal(true);
-                dialog.setAttribute(Qt::WA_NativeWindow, true);
-                KWindowSystem::setMainWindow(dialog.windowHandle(), winId);
-            }
-        }
+        dialog.setModal(true);
+        dialog.setAttribute(Qt::WA_NativeWindow, true);
+        KWindowSystem::setMainWindow(dialog.windowHandle(), attachHandle);
     }
 
     dialog.show();
